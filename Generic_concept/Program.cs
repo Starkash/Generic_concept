db8c306f894deeb0475fd0221122828a479df68c
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-28 00:45:40 +0530

    Add project files.

---
+++ Generic_concept/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Generic_concept
+{
+    class Pqr
+    {
+        public void xy<T>(T[] A)
+        {
+            foreach (T U in A)
+
+                Console.WriteLine("The value is " + U);
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Pqr B = new Pqr();
+            int[] A = { 1, 2, 5 };
+            string[] name = { "RAM", "SHYAM" };
+            B.xy(A);
+            B.xy(name);
+
+        }
+    }
+}
